fe654806edc684b001f39ba43783d14f8fc2d829
Randy Rebucas <rebucasrandy1986@gmail.com>
2019-05-24 22:46:41 -0700

    update default layout

--- application/views/default.php
+++ application/views/default.php
@@ -206,6 +206,17 @@
 
     <!-- Template Main Javascript File -->
     <script src="<?php echo base_url();?>assets/js/main.js"></script>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140514960-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'UA-140514960-1');
+    </script>
+
 </body>
 
 </html>
