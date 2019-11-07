7425cb12be7bbb330a0997948e0611ffdf4b6eb6
Randy Rebucas <rebucasrandy1986@gmail.com>
2019-11-07 17:30:43 +0800

    update to angular file only

--- backend/helpers/adapt-request.js
+++
@@ -1,9 +0,0 @@
-export default function adaptRequest (req = {}) {
-  return Object.freeze({
-    path: req.path,
-    method: req.method,
-    pathParams: req.params,
-    queryParams: req.query,
-    body: req.body
-  })
-}
