ca374de0b0219b5b2c0f03e0e56b33727bf2a7f1
Randy Rebucas <rebucasrandy1986@gmail.com>
2019-12-25 18:15:48 +0800

    update

---
+++ booking/.history/src/app/home/home.page_20191222161340.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  slideOpts: any;
+  constructor() {}
+
+  ngOnInit() {
+    this.slideOpts = {
+      initialSlide: 1,
+      speed: 400
+    };
+  }
+}
