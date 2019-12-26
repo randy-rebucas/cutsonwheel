f18cde37eda3f0e625b32b89e8b2f891b8a076ed
Randy Rebucas <rebucasrandy1986@gmail.com>
2019-12-26 17:11:43 +0800

    update

---
+++ booking/.history/src/app/home/home.page_20191226094022.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  slideOpts: any;
+
+  user: firebase.User;
+  token: firebase.auth.IdTokenResult;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.slideOpts = {
+      initialSlide: 1,
+      speed: 400
+    };
+
+    this.authService.getUserState()
+      .subscribe( user => {
+        this.user = user;
+        console.log(user.uid);
+      });
+
+    this.authService.getIdToken().subscribe((token) => {
+      console.log(token);
+    });
+  }
+}
