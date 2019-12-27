ddbc7386093bca937b9e1819718c5026df96bf3e
Randy Rebucas <rebucasrandy1986@gmail.com>
2019-12-27 22:20:07 +0800

    update profile

---
+++ booking/.history/src/app/home/home.page_20191227143508.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+
+import { Plugins, Capacitor } from '@capacitor/core';
+import { PlaceLocation, Coordinates } from '../services/location';
+import { AlertController } from '@ionic/angular';
+import { switchMap, map } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { UsersService } from '../users/users.service';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+  slideOpts: any;
+
+  user: firebase.User;
+  token: firebase.auth.IdTokenResult;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private alertCtrl: AlertController,
+    private http: HttpClient,
+    private userService: UsersService
+  ) {}
+
+  ngOnInit() {
+    this.slideOpts = {
+      initialSlide: 0,
+      speed: 400
+    };
+
+    // this.authService.getUserState()
+    //   .subscribe( user => {
+    //     if (user) {
+    //       this.router.navigateByUrl('/t/services/discover');
+    //     }
+    // });
+
+    this.locateUser();
+    // this.authService.getIdToken()
+    //   .subscribe((token) => {
+    //     console.log(token);
+    //   }
+    // );
+  }
+
+  private locateUser() {
+    if (!Capacitor.isPluginAvailable('Geolocation')) {
+      this.showErrorAlert();
+      return;
+    }
+    this.authService.getUserState().pipe(
+      map(user => {
+        return user;
+      })
+    ).subscribe((u) => {
+      if (u) {
+        Plugins.Geolocation.getCurrentPosition()
+          .then(geoPosition => {
+            const coordinates: Coordinates = {
+              lat: geoPosition.coords.latitude,
+              lng: geoPosition.coords.longitude
+            };
+            this.getAddress(coordinates.lat, coordinates.lng).subscribe(currentAddress => {
+              const user  = {
+                id: u.uid,
+                address: currentAddress,
+                latitude: coordinates.lat,
+                longitude: coordinates.lng
+              };
+              this.userService.setUserLocation(user).then(res => {
+                // this.router.navigate(['/t/places/discover']);
+              });
+            });
+          })
+          .catch(err => {
+            this.showErrorAlert();
+          });
+      }
+      console.log('user: ' + u.uid);
+    });
+
+    // this.authService.getUserState()
+    //   .subscribe( user => {
+    //     if (user) {
+
+    //     }
+    // });
+  }
+
+  private showErrorAlert() {
+    this.alertCtrl
+      .create({
+        header: 'Could not fetch location',
+        message: 'Please use the map to pick a location!',
+        buttons: ['Okay']
+      })
+      .then(alertEl => alertEl.present());
+  }
+
+  private getAddress(lat: number, lng: number) {
+    return this.http
+      .get<any>(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${
+          environment.googleMapsApiKey
+        }`
+      )
+      .pipe(
+        map(geoData => {
+          if (!geoData || !geoData.results || geoData.results.length === 0) {
+            return null;
+          }
+          return geoData.results[0].formatted_address;
+        })
+      );
+  }
+}
